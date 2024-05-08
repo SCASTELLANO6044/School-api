e471eb34868d5ba6e5d5387a4d0785f39e4a5faa
SCASTELLANO6045 <sergiocastellanotrabajo@gmail.com>
2024-05-08 13:26:42 +0200

    :tada: Initialize project

---
+++ school-api/src/test/java/com/porsche/schoolapi/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porsche.schoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
